a0479c065fec02d37c6f0b14ab4f9cc4876c44a4
leochanj105 <leocjy105@gmail.com>
2024-07-14 03:41:07 -0700

    add small bench

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #build
 
 JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 mvn clean package -Pdist -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 mvn test-compile
